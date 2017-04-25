5c8486e6789d5f1cb544e84785a9cd6510efbc8a
Robert Springer <springerdrums@gmail.com>
2017-04-24 19:41:28 -0700

    fixed links

--- src/sections/One.js
+++ src/sections/One.js
@@ -10,8 +10,8 @@ class One extends Component{
             <h4>Development/Design</h4>
           </div>
           <div className="social-wrapper">
-            <i className="fa fa-social fa-github" aria-hidden="true"></i>
-            <i className="fa fa-social fa-linkedin" aria-hidden="true"></i>
+            <a href="http://github.com/ianspringer"><i className="fa fa-social fa-github" aria-hidden="true"></i></a>
+            <a href="http://linkedin.com/in/ianspringer"><i className="fa fa-social fa-linkedin" aria-hidden="true"></i></a>
           </div>
         </div>
       </div>
